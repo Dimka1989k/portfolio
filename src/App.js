5e0ce26647f493a290973e0a44d2745a11cfd0f0
Dimon1989k <106411942+Dimon1989k@users.noreply.github.com>
2023-08-22 11:57:04 +0300

    change

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/myportfolio" element={<Portfolio />} />
         <Route path="/resume" element={<Resume />} />
       </Routes>
     </div>
