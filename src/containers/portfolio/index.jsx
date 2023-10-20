78c25debb282e495c7c46c7e1439208b28141685
Dimon1989k <106411942+Dimon1989k@users.noreply.github.com>
2023-10-20 17:31:08 +0300

    change styles

--- src/containers/portfolio/index.jsx
+++ src/containers/portfolio/index.jsx
@@ -9,7 +9,7 @@ import image5 from "../../images/Kapusta.jpg";
 import image6 from "../../images/Movie.jpg";
 import image7 from "../../images/Petly.jpg";
 import image8 from "../../images/PhoneBook.jpg";
-import image9 from "../../images/WebStudio.jpg";
+import image9 from "../../images/WebStudio.png";
 import image10 from "../../images/Countries.png";
 import image11 from "../../images/Weather.png";
 import image12 from "../../images/hounter.png";
@@ -21,10 +21,23 @@ import "./styles.scss";
 const portfolioData = [
   {
     id: 2,
+    name: "New Post",
+    image: image14,
+    link: "https://dimon1989k.github.io/test-sameit/",
+    linkHithub: "https://github.com/Dimon1989k/test-sameit",
+    description:
+      "The application for searching branch locations and their shedule, as well as tracking the status of shipment tracking numbers (TTN).",
+    tech: "[React, RTK, Styled-Components]",
+  },
+  {
+    id: 2,
     name: "EventBooster",
     image: image2,
     link: "https://jekiekolya.github.io/Command-project-JS/index.html",
     linkHithub: "https://github.com/Dimon1989k/goit-markup-hw-08",
+    description:
+      "The application for ordering concert tickets. Role: Developer. Logic and stylization of the footer (pagination).",
+    tech: "[HTML, SASS, JS]",
   },
 
   {
@@ -33,6 +46,9 @@ const portfolioData = [
     image: image3,
     link: "https://nikolaylyah.github.io/goit-final/",
     linkHithub: "https://github.com/Nikolaylyah/goit-final",
+    description:
+      "The website with a responsive layout. Role: Developer. Layout and stylization of the header and mobile menu.",
+    tech: "[HTML, SASS, JS]",
   },
   {
     id: 2,
@@ -40,6 +56,8 @@ const portfolioData = [
     image: image4,
     link: "https://dimon1989k.github.io/goit-react-hw-03-image-finder/",
     linkHithub: "https://github.com/Dimon1989k/goit-react-hw-04-images",
+    description: "Image search app",
+    tech: "[REACT, CSS]",
   },
   {
     id: 3,
@@ -47,6 +65,9 @@ const portfolioData = [
     image: image5,
     link: "https://hodosovsky.github.io/kapusta/login",
     linkHithub: "https://github.com/hodosovsky/kapusta",
+    description:
+      "The application to control your personal budget. Role: Developer. Logic and stylization of the login and registration window. Phonebook - The application to save search and delete phone numbers.",
+    tech: "[React, RTK, Styled-Components, Swagger]",
   },
   {
     id: 2,
@@ -54,6 +75,8 @@ const portfolioData = [
     image: image6,
     link: "https://dimon1989k.github.io/goit-react-hw-05-movies/",
     linkHithub: "https://github.com/Dimon1989k/goit-react-hw-05-movies",
+    description: "The application for searching favorite movies",
+    tech: "[React, CSS]",
   },
   {
     id: 3,
@@ -61,6 +84,9 @@ const portfolioData = [
     image: image7,
     link: "https://petly-project-app.netlify.app/",
     linkHithub: "https://github.com/volodymyr-ok/petly-project",
+    description:
+      "The application for searching for buying and selling pets. Role: Developer. Logic and stylization of the login and registration window, home page, cards and a modal window for adding pets.",
+    tech: "[React, RTK, Styled-Components, Swagger, Node]",
   },
   {
     id: 2,
@@ -68,6 +94,8 @@ const portfolioData = [
     image: image8,
     link: "https://dimon1989k.github.io/goit-react-hw-08-phonebook/",
     linkHithub: "https://github.com/Dimon1989k/goit-react-hw-08-phonebook",
+    description: "The application to save search and delete phone numbers.",
+    tech: "[React, CSS, RTK]",
   },
   {
     id: 3,
@@ -75,6 +103,8 @@ const portfolioData = [
     image: image9,
     link: "https://dimon1989k.github.io/goit-markup-hw-08/",
     linkHithub: "https://github.com/Dimon1989k/goit-markup-hw-08",
+    description: "The simple landing page with apaptive layout design.",
+    tech: "[HTML, CSS, SASS]",
   },
   {
     id: 2,
@@ -82,6 +112,8 @@ const portfolioData = [
     image: image10,
     link: "https://dimon1989k.github.io/goit-js-hw-10/",
     linkHithub: "https://github.com/Dimon1989k/goit-js-hw-10",
+    description: "The application for searching countries.",
+    tech: "[HTML, CSS, JS]",
   },
   {
     id: 2,
@@ -89,6 +121,8 @@ const portfolioData = [
     image: image11,
     link: "https://dimon1989k.github.io/weather/",
     linkHithub: "https://github.com/Dimon1989k/weather",
+    description: "Simple weather application.",
+    tech: "[React, CSS]",
   },
   {
     id: 3,
@@ -96,6 +130,8 @@ const portfolioData = [
     image: image12,
     link: "https://dimon1989k.github.io/test-remote/",
     linkHithub: "https://github.com/Dimon1989k/test-remote",
+    description: "The simple landing page with apaptive layout design.",
+    tech: "[React, Styled-Components]",
   },
 
   {
@@ -104,14 +140,8 @@ const portfolioData = [
     image: image13,
     link: "https://dimon1989k.github.io/test-Smart/",
     linkHithub: "https://github.com/Dimon1989k/test-Smart",
-  },
-
-  {
-    id: 2,
-    name: "Нова Пошта",
-    image: image14,
-    link: "https://dimon1989k.github.io/test-sameit/",
-    linkHithub: "https://github.com/Dimon1989k/test-sameit",
+    description: "The simple landing page with apaptive layout design.",
+    tech: "[React, Styled-Components]",
   },
 ];
 
@@ -153,6 +183,8 @@ const Portfolio = () => {
                       <button onClick={() => window.open(item.linkHithub)}>
                         VISIT GITHUB
                       </button>
+                      <h1>{item.description}</h1>
+                      <h1>{item.tech}</h1>
                     </div>
                   )}
                 </div>
